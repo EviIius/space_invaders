4b197cc5805407a6f3b1ac1f85ecf9bdb13ab83c
Jake Brulato <jakebrulato@gmail.com>
2024-03-20 18:08:23 -0400

    started

--- canvas/index.js
+++ canvas/index.js
@@ -1,1 +1,31 @@
-// beau is dumb
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+class Player{
+    constructor(){
+        this.position = {
+            x: 200,
+            y: 200
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.image = new Image();
+        this.width = 100;
+        this.height = 100;
+    }
+    draw(){
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const player = new Player();
+player.draw();
